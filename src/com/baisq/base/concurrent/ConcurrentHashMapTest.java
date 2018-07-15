a7d6796792843c9a55e5c380dafc9cad18ef36a7
fontain1128 <fontain1128@163.com>
2018-07-15 11:17:36 +0800

    test

--- src/com/baisq/base/concurrent/ConcurrentHashMapTest.java
+++ src/com/baisq/base/concurrent/ConcurrentHashMapTest.java
@@ -12,6 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ConcurrentHashMapTest {
 
+
 	public static void main(String[] args) {
 		ConcurrentHashMap map = new ConcurrentHashMap<>();
 		
